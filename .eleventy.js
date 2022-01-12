ef16c6642828f635401df3a5473ba2109020f7b5
Jason Carrasco <5018598+jtcarrasco@users.noreply.github.com>
2022-01-12 09:27:40 -0800

    20220112 update to 11ty sass template

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,12 @@
-module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addPassthroughCopy('./src/assets'); // assets in public folder
-  eleventyConfig.addPassthroughCopy('./src/admin'); // assets in public folder
-  eleventyConfig.setBrowserSyncConfig({
-		files: './public/css/**/*.css'
-	});
-
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  // eleventyConfig.addPassthroughCopy("./src/img");
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
-  }
+      output: "public",
+    },
+  };
 };
